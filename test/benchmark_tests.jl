d4e6ccc112028e9cce1c573c39519be362ac59c4
ChrisRackauckas <me@chrisrackauckas.com>
2016-11-10 13:30:56 -0800

    tests pass

--- test/benchmark_tests.jl
+++ test/benchmark_tests.jl
@@ -1,10 +1,10 @@
-using OrdinaryDiffEq
+using OrdinaryDiffEq, DiffEqDevTools, DiffEqProblemLibrary, DiffEqBase
 
 ## Setup Tests
 
 prob = prob_ode_linear
 
-probs = Vector{ODEProblem}(2)
+probs = Vector{ODETestProblem}(2)
 probs[1] = prob
 probs[2] = prob_ode_2Dlinear
 
@@ -15,12 +15,12 @@ abstols = 1./10.^(3:10)
 reltols = 1./10.^(3:10)
 
 
-setups = [Dict(:alg=>:RK4);Dict(:alg=>:Euler);Dict(:alg=>:BS3);
-          Dict(:alg=>:Midpoint);Dict(:alg=>:BS5);Dict(:alg=>:DP5)]
+setups = [Dict(:alg=>RK4);Dict(:alg=>Euler);Dict(:alg=>BS3);
+          Dict(:alg=>Midpoint);Dict(:alg=>BS5);Dict(:alg=>DP5)]
 
 
-t1 = @elapsed sol = solve(prob::ODEProblem,tspan,alg=:RK4,dt=1/2^(4))
-t2 = @elapsed sol2 = solve(prob::ODEProblem,tspan;dt=1/2^(4),setups[1]...)
+t1 = @elapsed sol = solve(prob,RK4,dt=1/2^(4))
+t2 = @elapsed sol2 = solve(prob,setups[1][:alg],dt=1/2^(4))
 
 bool = (sol2[end] == sol[end])
 
@@ -28,13 +28,13 @@ bool = (sol2[end] == sol[end])
 
 println("Shootout Tests")
 
-shoot = ode_shootout(prob,tspan,setups,dt=1/2^(4))
+shoot = ode_shootout(prob,setups,dt=1/2^(4))
 
 show(shoot)
 println(shoot)
 shoot[end]
 
-set = ode_shootoutset(probs,tspans,setups;dt=1/2^(4))
+set = ode_shootoutset(probs,setups;dt=1/2^(4))
 
 println(set[1])
 println(set[:])
@@ -43,14 +43,14 @@ set[1][:]
 
 ## WorkPrecision Tests
 println("WorkPrecision Tests")
-wp = ode_workprecision(prob,tspan,abstols,reltols;alg=:DP5,name="Dormand-Prince 4/5")
+wp = ode_workprecision(prob,DP5,abstols,reltols;name="Dormand-Prince 4/5")
 
 wp[1]
 wp[:]
 wp[end]
 show(wp)
 
-wp_set = ode_workprecision_set(prob,tspan,abstols,reltols,setups;dt=1/2^4,numruns=2)
+wp_set = ode_workprecision_set(prob,abstols,reltols,setups;dt=1/2^4,numruns=2)
 
 wp_set[1]
 wp_set[:]
