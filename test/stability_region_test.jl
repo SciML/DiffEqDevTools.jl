371763aac64ab1001f5758cf47a8c67c41b19347
YingboMa <mayingbo5@gmail.com>
2018-03-28 15:00:26 -0700

    Add a method to compute the length of stability regions

---
+++ test/stability_region_test.jl
@@ -0,0 +1,5 @@
+using DiffEqDevTools, Base.Test
+
+@test stability_region(constructDormandPrince6(), initial_guess=-3.5) + 3.95413 < 1e-3
+@test stability_region(constructTsitourasPapakostas6(), initial_guess=-3.5) + 3.95413 < 1e-3
+@test stability_region(constructRadauIIA5(), initial_guess=12.) - 11.84 < 1e-2
