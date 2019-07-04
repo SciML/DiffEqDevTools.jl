c381b1ab0ebcac2942eba52b0f9c8b10b69d2dbe
David Widmann <dev+git@devmotion.de>
2019-07-04 16:01:26 +0200

    Update call of analytic function

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -380,7 +380,7 @@ end
     else
       W = sqrt(_dt)*randn(size(prob.u0))
     end
-    analytical_solution_ends[i] = norm(prob.f(Val{:analytic},prob.u0,prob.p,prob.tspan[2],W))
+    analytical_solution_ends[i] = norm(prob.f.analytic(prob.u0,prob.p,prob.tspan[2],W))
   end
 end
 
