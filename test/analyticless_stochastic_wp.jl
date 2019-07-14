f707fed940ada5b18a9905f52dd38650400ca3b8
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-07-14 05:59:06 -0400

    make test slightly easier on Travis

--- test/analyticless_stochastic_wp.jl
+++ test/analyticless_stochastic_wp.jl
@@ -5,7 +5,7 @@ using DiffEqProblemLibrary.SDEProblemLibrary: prob_sde_additivesystem
 prob = prob_sde_additivesystem
 prob = SDEProblem(prob.f,prob.g,prob.u0,(0.0,0.1),prob.p)
 
-reltols = 1.0./10.0.^(1:5)
+reltols = 1.0./10.0.^(1:4)
 abstols = reltols#[0.0 for i in eachindex(reltols)]
 setups = [Dict(:alg=>SRIW1())
           Dict(:alg=>EM(),:dts=>1.0./5.0.^((1:length(reltols)) .+ 1),:adaptive=>false)
@@ -19,14 +19,14 @@ test_dt = 0.1
 wp = WorkPrecisionSet(prob,abstols,reltols,setups,test_dt;
                                      numruns=5,names=names,error_estimate=:l2)
 
-se = get_sample_errors(prob,setups[1],numruns=100,solution_runs=200)
-se = get_sample_errors(prob,setups[1],numruns=[5,10,25,50,100,1000],solution_runs=200)
+se = get_sample_errors(prob,setups[1],numruns=100,solution_runs=100)
+se = get_sample_errors(prob,setups[1],numruns=[5,10,25,50,100,1000],solution_runs=100)
 
 println("Now weak error without analytical solution")
 
 prob2 = SDEProblem((du,u,p,t)->prob.f(du,u,p,t),prob.g,prob.u0,(0.0,0.1),prob.p)
-test_dt = 1/10^5
-appxsol_setup = Dict(:alg=>SRIW1(),:abstol=>1e-5,:reltol=>1e-5)
+test_dt = 1/10^4
+appxsol_setup = Dict(:alg=>SRIW1(),:abstol=>1e-4,:reltol=>1e-4)
 wp = WorkPrecisionSet(prob2,abstols,reltols,setups,test_dt;
                                      appxsol_setup = appxsol_setup,
                                      numruns=5,names=names,error_estimate=:weak_final)
