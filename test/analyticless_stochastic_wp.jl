07fba9b4abc651c5f0e907c723f070a35ae466f4
Yingbo Ma <mayingbo5@gmail.com>
2020-07-14 23:05:24 -0400

    names -> _names fix test failure

--- test/analyticless_stochastic_wp.jl
+++ test/analyticless_stochastic_wp.jl
@@ -14,10 +14,10 @@ setups = [Dict(:alg=>SRIW1())
           Dict(:alg=>SRA1(),:dts=>1.0./5.0.^((1:length(reltols)) .+ 1),:adaptive=>false)
           Dict(:alg=>SRA1())
           ]
-names = ["SRIW1","EM","RKMil","SRIW1 Fixed","SRA1 Fixed","SRA1"]
+_names = ["SRIW1","EM","RKMil","SRIW1 Fixed","SRA1 Fixed","SRA1"]
 test_dt = 0.1
 wp = WorkPrecisionSet(prob,abstols,reltols,setups,test_dt;
-                                     numruns=5,names=names,error_estimate=:l2)
+                                     numruns=5,names=_names,error_estimate=:l2)
 
 se = get_sample_errors(prob,setups[1],numruns=100,solution_runs=100)
 se = get_sample_errors(prob,setups[1],numruns=[5,10,25,50,100,1000],solution_runs=100)
@@ -29,7 +29,7 @@ test_dt = 1/10^4
 appxsol_setup = Dict(:alg=>SRIW1(),:abstol=>1e-4,:reltol=>1e-4)
 wp = WorkPrecisionSet(prob2,abstols,reltols,setups,test_dt;
                                      appxsol_setup = appxsol_setup,
-                                     numruns=5,names=names,error_estimate=:weak_final)
+                                     numruns=5,names=_names,error_estimate=:weak_final)
 
 println("Get sample errors")
 
