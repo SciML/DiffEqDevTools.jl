3523fe8443a32d2624eee25f177b6ddc82ccc1eb
Ricardo Rosa <rmsrosa@gmail.com>
2022-09-07 22:53:20 -0300

    rename `names`

--- test/plotrecipes_tests.jl
+++ test/plotrecipes_tests.jl
@@ -26,15 +26,16 @@ reltols = 1.0 ./ 10.0 .^ (0:5)
 
 setups = [Dict(:alg => DP5())
           Dict(:alg => Tsit5())]
-names = ["DP5", "Tsit5"]
-wp = WorkPrecisionSet(prob, abstols, reltols, setups, names = names, save_everystep = false,
+wp_names = ["DP5", "Tsit5"]
+wp = WorkPrecisionSet(prob, abstols, reltols, setups, names = wp_names,
+                      save_everystep = false,
                       numruns = 100)
 
 plt = @test_nowarn plot(wp)
 @test plt[1][1][:x] ≈ wp[1].errors
 @test plt[1][2][:x] ≈ wp[2].errors
-@test plt[1][1][:label] == names[1]
-@test plt[1][2][:label] == names[2]
+@test plt[1][1][:label] == wp_names[1]
+@test plt[1][2][:label] == wp_names[2]
 @test_nowarn plot(wp, color = [1 2])
 @test_nowarn plot(wp, color = :blue)
 @test_throws ArgumentError plot(wp, view = :dt_convergence)
@@ -44,18 +45,19 @@ setups = [Dict(:alg => Euler(), :dts => dts)
           Dict(:alg => Heun(), :dts => dts)
           Dict(:alg => Tsit5(), :dts => dts, :adaptive => false)
           Dict(:alg => Tsit5())]
-names = ["Euler", "Heun", "Tsit5 fixed step", "Tsit5 adaptive"]
-wp = WorkPrecisionSet(prob, abstols, reltols, setups, names = names, save_everystep = false,
+wp_names = ["Euler", "Heun", "Tsit5 fixed step", "Tsit5 adaptive"]
+wp = WorkPrecisionSet(prob, abstols, reltols, setups, names = wp_names,
+                      save_everystep = false,
                       numruns = 100)
 
 plt = @test_nowarn plot(wp)
 @test all(plt[1][i][:x] ≈ wp[i].errors for i in 1:4)
-@test all(plt[1][i][:label] == names[i] for i in 1:4)
+@test all(plt[1][i][:label] == wp_names[i] for i in 1:4)
 
 plt = @test_nowarn plot(wp, view = :dt_convergence, legend = :bottomright)
 @test all(plt[1][i][:x] == plt[1][i + 3][:x] == dts == wp.setups[i][:dts] for i in 1:3)
 @test all(plt[1][i + 3][:y] ≈ wp[i].errors for i in 1:3)
-@test all(startswith(plt[1][i + 3][:label], names[i]) for i in 1:3)
+@test all(startswith(plt[1][i + 3][:label], wp_names[i]) for i in 1:3)
 @test_throws BoundsError plt[1][7]
 @test_nowarn plot(wp, view = :dt_convergence, color = [:red :orange :green])
 @test_nowarn plot(wp, view = :dt_convergence, color = :lightblue, title = "Δt Convergence")
