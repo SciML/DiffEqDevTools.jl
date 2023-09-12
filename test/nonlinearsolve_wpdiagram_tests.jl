995b76894bc767ee4aa2e7c3cbcd958768fe6193
Torkel <torkel.loman@gmail.com>
2023-09-12 18:45:50 -0400

    encapsule test in let ... end statement

--- test/nonlinearsolve_wpdiagram_tests.jl
+++ test/nonlinearsolve_wpdiagram_tests.jl
@@ -2,21 +2,23 @@
 using NonlinearSolve, DiffEqDevTools, Plots
 
 # Prepares NonlinearProblem.
-f(u, p) = u .* u .- p
-u0 = [1.0, 1.0]
-p = 2.0
-static_prob = NonlinearProblem(f, u0, p)
-real_sol = solve(static_prob, NewtonRaphson(), reltol = 1e-15, abstol = 1e-15)
+let
+    f(u, p) = u .* u .- p
+    u0 = [1.0, 1.0]
+    p = 2.0
+    static_prob = NonlinearProblem(f, u0, p)
+    real_sol = solve(static_prob, NewtonRaphson(), reltol = 1e-15, abstol = 1e-15)
 
-# Sets WP input.
-abstols = 1.0 ./ 10.0 .^ (8:12)
-reltols = 1.0 ./ 10.0 .^ (8:12)
-setups = [Dict(:alg=>NewtonRaphson())
-          Dict(:alg=>TrustRegion())]
-solnames = ["NewtonRaphson";"TrustRegion"]
+    # Sets WP input.
+    abstols = 1.0 ./ 10.0 .^ (8:12)
+    reltols = 1.0 ./ 10.0 .^ (8:12)
+    setups = [Dict(:alg=>NewtonRaphson())
+            Dict(:alg=>TrustRegion())]
+    solnames = ["NewtonRaphson";"TrustRegion"]
 
-# Makes WP-diagram
-wp = WorkPrecisionSet(static_prob, abstols, reltols, setups; names=solnames, numruns=100, appxsol=real_sol, error_estimate=:l2) 
+    # Makes WP-diagram
+    wp = WorkPrecisionSet(static_prob, abstols, reltols, setups; names=solnames, numruns=100, appxsol=real_sol, error_estimate=:l2) 
 
-# Checks that all errors are small (they definitely should be).
-all(vcat(getfield.(wp.wps, :errors)...) .< 10e-9)
+    # Checks that all errors are small (they definitely should be).
+    all(vcat(getfield.(wp.wps, :errors)...) .< 10e-9)
+end
